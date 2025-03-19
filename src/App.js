fcc05d219dcd7c5ae28b30d030f4d23995398e8f
CecilieMari <95425622+CecilieMari@users.noreply.github.com>
2025-03-19 14:44:16 +0100

    adding the navbar and router and layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import SingleProduct from './components/singleProduct/singelProduct';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Router>
+     <Layout> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<SingleProduct />} /> {/* SingleProduct*/}
       </Routes>
+     </Layout>
     </Router>
   );
 }
