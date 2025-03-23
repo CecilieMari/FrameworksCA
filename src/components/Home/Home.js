f646f018a27d110e27fbe2265d926e3d7dc792cf
CecilieMari <95425622+CecilieMari@users.noreply.github.com>
2025-03-23 16:21:48 +0100

    adding the src bar at the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import ProductCard from '../ProductCard/ProductCard';
 import Styles from './Home.module.css';
 import { CartContext } from '../cart/CartContext';
+import SearchBar from '../search/SearchBar';
 
 const url = 'https://v2.api.noroff.dev/online-shop';
 
@@ -42,6 +43,7 @@ function Home() {
   return (
     <div>
       <h1>Product</h1>
+      <SearchBar products={products} />
       <div className={Styles.productGrid}>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} addToCart={addToCart} />
